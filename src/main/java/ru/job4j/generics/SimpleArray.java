6db8b2e1d07dca5e0cd135546631cc9089d8fd7a
Insomn1ac <emelyanoff94@gmail.com>
2021-07-23 22:56:14 +0300

    Реализовать SimpleArray<T> [#156]

---
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -0,0 +1,55 @@
+package ru.job4j.generics;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] array;
+    private int size;
+
+    public SimpleArray() {
+        array = new Object[10];
+    }
+
+    public void add(T model) {
+        array[size] = model;
+        size++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return (T) array[index];
+    }
+
+    public void set(int index, T model) {
+        Objects.checkIndex(index, size);
+        array[index] = model;
+    }
+
+    public void remove(int index) {
+        Objects.checkIndex(index, size);
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        size--;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            private int point = 0;
+
+            @Override
+            public boolean hasNext() {
+                return point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) array[point++];
+            }
+        };
+    }
+}
---
+++ src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -0,0 +1,65 @@
+package ru.job4j.generics;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class SimpleArrayTest {
+
+    @Test
+    public void whenAddThenGet() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("first");
+        String rsl = array.get(0);
+        assertThat(rsl, is("first"));
+    }
+
+    @Test
+    public void whenAddThenIt() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("first");
+        String rsl = array.iterator().next();
+        assertThat(rsl, is("first"));
+    }
+
+    @Test
+    public void whenRemove() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("first");
+        array.add("second");
+        array.remove(0);
+        assertThat(array.get(0), is("second"));
+    }
+
+    @Test
+    public void whenSet() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("first");
+        array.add("second");
+        array.set(1, "fourth");
+        assertEquals(array.get(1), "fourth");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetEmpty() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.get(0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetOutBound() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.add("first");
+        array.get(1);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenGetEmptyFromIt() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.iterator().next();
+    }
+
+}
