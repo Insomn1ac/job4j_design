8b0054804543e122e5677c927855b9309753cdac
Insomn1ac <emelyanoff94@gmail.com>
2021-08-12 21:01:44 +0300

    Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.collection.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +14,23 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children + '\''
+                + ", hashCode()=" + this.hashCode() + '}';
+    }
+
+    public static void main(String[] args) {
+        Calendar ivanBirth = new GregorianCalendar(1983, Calendar.NOVEMBER, 24);
+        User ivan = new User("Ivan", 2, ivanBirth);
+        User ivanTheSecond = new User("Ivan", 2, ivanBirth);
+        Map<User, Object> userObjectMap = new HashMap<>();
+        userObjectMap.put(ivan, new Object());
+        userObjectMap.put(ivanTheSecond, new Object());
+        System.out.println(userObjectMap);
     }
 }
