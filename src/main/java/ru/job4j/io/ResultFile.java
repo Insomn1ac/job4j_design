350c34acdb45ebf11f1d214272b04fb7bc9124e0
Insomn1ac <emelyanoff94@gmail.com>
2021-08-27 13:29:46 +0300

    FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static int[][] multiple(int size) {
+        int[][] array = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = multiple(10);
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int[] row : array) {
+                for (int el : row) {
+                    if (el / 10 < 1) {
+                        out.write(("  " + el).getBytes());
+                    } else {
+                        out.write((" " + el).getBytes());
+                    }
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
