23a166d39eafcde455c6bc8fcb7a2e7425977639
Insomn1ac <emelyanoff94@gmail.com>
2021-09-09 14:25:36 +0300

    Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,10 +9,16 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar search.jar ROOT_FOLDER.");
+        }
+        if (args.length < 2 || args[1].isEmpty()) {
+            throw new IllegalArgumentException("Search query is null. Enter your search query as an argument in cmd/shell");
+        }
+        Path start = Paths.get(args[0]);
         search(start, p -> p.toFile()
                 .getName()
-                .startsWith("Search"))
+                .startsWith(args[1]))
                 .forEach(System.out::println);
     }
 
