1dfceeb7a7d3b9da046ad0f8b6df1dcfc3329bde
Insomn1ac <emelyanoff94@gmail.com>
2021-09-17 23:24:22 +0300

    Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,11 +1,17 @@
 package ru.job4j.io;
 
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -30,8 +36,12 @@ public class EchoServer {
                         }
                     }
                     out.flush();
+                } catch (IOException e) {
+                    LOG.error("An error occurred when trying to open I/O stream in socket", e);
                 }
             }
+        } catch (IOException e) {
+            LOG.error("An error occurred when trying to create new server socket", e);
         }
     }
 }
