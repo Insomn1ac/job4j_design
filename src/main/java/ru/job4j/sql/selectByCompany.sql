e2b797c6fac893e63e6dc2afbc9699e86544494e
Insomn1ac <emelyanoff94@gmail.com>
2021-10-30 20:47:04 +0300

    SQL Select by company [#2760] additional question

--- src/main/java/ru/job4j/sql/selectByCompany.sql
+++ src/main/java/ru/job4j/sql/selectByCompany.sql
@@ -10,3 +10,11 @@ from (select c.name as "Название компании", count(p.id) as "Ко
 	group by c.name) as subtable
 order by "Количество сотрудников" desc
 limit 1;
+
+select c.name as "Название компании", count(p.id) as "Количество сотрудников"
+from company as c
+join person as p on c.id = p.company_id
+group by c.name
+having count(p.id) >= all(select count(p.id) from person as p
+						  join company as c on p.company_id = c.id
+						  group by c.name);
