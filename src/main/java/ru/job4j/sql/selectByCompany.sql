607d7477f1cbc599cf8e09ba90a7de5632826e77
Insomn1ac <emelyanoff94@gmail.com>
2021-10-30 19:11:10 +0300

    SQL Select by company [#2760]

---
+++ src/main/java/ru/job4j/sql/selectByCompany.sql
@@ -0,0 +1,12 @@
+select p.name as "Имя сотрудника", c.name as "Название компании"
+from person as p
+join company as c on p.company_id = c.id
+where c.id <> 5;
+
+select "Название компании", "Количество сотрудников"
+from (select c.name as "Название компании", count(p.id) as "Количество сотрудников"
+	from company as c
+	join person as p on c.id = p.company_id
+	group by c.name) as subtable
+order by "Количество сотрудников" desc
+limit 1;
